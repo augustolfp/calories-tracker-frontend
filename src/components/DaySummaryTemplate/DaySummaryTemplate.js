7a8792b8c97d17d1feaf65fa4656a8d04d0da22b
Augusto Lopes <augustolfp@gmail.com>
2022-10-08 17:43:02 -0300

    feat(User homepage): Add weekday to Counted-day template

--- src/components/DaySummaryTemplate/DaySummaryTemplate.js
+++ src/components/DaySummaryTemplate/DaySummaryTemplate.js
@@ -1,13 +1,28 @@
 import styled from 'styled-components';
-
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+dayjs.extend(weekday);
+const daysInPtBr = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado'
+];
 export default function DaySummaryTemplate(props) {
+  const formattedDay = dayjs(props.day).format('DD-MM-YYYY');
+  const weekDay = daysInPtBr[dayjs(props.day).weekday()];
   return (
     <Container>
-      <h2>{props.day}</h2>
-      <h3>Energia: {props.kcals}</h3>
-      <h4>Proteinas: {props.proteins}</h4>
-      <h4>Gorduras: {props.fats}</h4>
-      <h4>Carbohidratos: {props.carbs}</h4>
+      <h2>{formattedDay}</h2>
+      <h2>{weekDay}</h2>
+      <h3>Energia: {props.kcals} kCal</h3>
+      <h4>Proteinas: {props.proteins} g</h4>
+      <h4>Gorduras: {props.fats} g</h4>
+      <h4>Carbohidratos: {props.carbs} g</h4>
+      <h5>Anotações: {props.notes}</h5>
     </Container>
   );
 }
@@ -15,6 +30,7 @@ export default function DaySummaryTemplate(props) {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 300px;
   border: solid 1px;
+  padding: 5px;
+  margin: 5px;
 `;

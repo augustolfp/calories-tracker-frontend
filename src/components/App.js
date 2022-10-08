6925f550281b797dbb9e87cc9b7eed3fce35596a
Augusto Lopes <augustolfp@gmail.com>
2022-10-08 12:37:05 -0300

    feat(Routes): Configure pages routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,18 @@
-import "../assets/styles/reset.css";
+import '../assets/styles/reset.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import SignInPage from './SignInPage/SignInPage';
+import SignUpPage from './SignUpPage/SignUpPage';
+import HomePage from './HomePage/HomePage';
 
 export default function App() {
-  return <h1>MyApp!!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

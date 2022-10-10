2d3b03f70f06c8e648c5a9a96de7c486c4e5c302
Augusto Lopes <augustolfp@gmail.com>
2022-10-10 14:08:15 -0300

    feat(Ingredient creation): Implement ingredient creation basic functions

---
+++ src/components/Templates/CreateIngredientTemplate/CreateIngredientTemplate.js
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+import React from 'react';
+import UserContext from '../../../contexts/UserContext';
+import axios from 'axios';
+
+export default function CreateIngredientTemplate(props) {
+  const { token, API_URL } = React.useContext(UserContext);
+  const [ingName, setIngName] = React.useState('');
+  const [weight, setWeight] = React.useState('');
+  const [carbs, setCarbs] = React.useState('');
+  const [fats, setFats] = React.useState('');
+  const [proteins, setProteins] = React.useState('');
+  const [kcals, setKcals] = React.useState('');
+  const [isDisabled, setIsDisabled] = React.useState(false);
+
+  function handleIngredientCreation(event) {
+    event.preventDefault();
+    setIsDisabled(true);
+
+    const body = {
+      ingredients: [
+        {
+          name: ingName,
+          mealId: props.mealId,
+          weightInGrams: Number(weight),
+          carbsInGrams: Number(carbs),
+          fatsInGrams: Number(fats),
+          proteinsInGrams: Number(proteins),
+          kcals: Number(kcals)
+        }
+      ]
+    };
+
+    const submitIngredient = axios.post(`${API_URL}/add-ingredients`, body, token);
+    console.log(body);
+    submitIngredient.then((response) => {
+      console.log('sucesso!');
+      console.log(response);
+      setIsDisabled(false);
+    });
+
+    submitIngredient.catch((error) => {
+      console.log('Falhou!');
+      console.log(error);
+      setIsDisabled(false);
+    });
+  }
+
+  return (
+    <Container>
+      <h2>Adicionar Ingrediente</h2>
+      <form onSubmit={handleIngredientCreation}>
+        <input
+          type="text"
+          name="ingName"
+          placeholder="Nome do ingrediente"
+          value={ingName}
+          disabled={isDisabled}
+          onChange={(e) => setIngName(e.target.value)}
+        />
+        <input
+          type="number"
+          name="weight"
+          value={weight}
+          placeholder="peso em g"
+          disabled={isDisabled}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+        <input
+          type="number"
+          name="carbs"
+          value={carbs}
+          placeholder="carbos em g"
+          disabled={isDisabled}
+          onChange={(e) => setCarbs(e.target.value)}
+        />
+        <input
+          type="number"
+          name="fats"
+          value={fats}
+          placeholder="gorduras em g"
+          disabled={isDisabled}
+          onChange={(e) => setFats(e.target.value)}
+        />
+        <input
+          type="number"
+          name="proteins"
+          value={proteins}
+          placeholder="proteinas em g"
+          disabled={isDisabled}
+          onChange={(e) => setProteins(e.target.value)}
+        />
+        <input
+          type="number"
+          name="kcals"
+          value={kcals}
+          placeholder="kcals em g"
+          disabled={isDisabled}
+          onChange={(e) => setKcals(e.target.value)}
+        />
+        <button type="submit" disabled={isDisabled}>
+          CRIAR
+        </button>
+      </form>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
--- src/components/Templates/MealTemplate/MealTemplate.js
+++ src/components/Templates/MealTemplate/MealTemplate.js
@@ -1,30 +1,40 @@
 import styled from 'styled-components';
 import IngredientTemplate from '../IngredientTemplate/IngredientTemplate';
+import React from 'react';
+import CreateIngredientTemplate from '../CreateIngredientTemplate/CreateIngredientTemplate';
 
 export default function MealTemplate(props) {
+  const [viewIngredientCreator, setViewIngredientCreator] = React.useState(false);
   return (
     <Container>
-      <h2>Refeição: {props.mealName}</h2>
-      <h2>Descrição: {props.mealDescription}</h2>
-      <h2>Carbohidratos: {props.carbs} g</h2>
-      <h2>Proteínas: {props.proteins} g</h2>
-      <h2>Gorduras: {props.fats} g</h2>
-      <h2>Calorias: {props.kcals} kCal</h2>
-      {props.ingredientList[0] !== null && props.ingredientList.length > 0 ? (
-        props.ingredientList.map((ingredient, index) => (
-          <IngredientTemplate key={index} {...ingredient} />
-        ))
-      ) : (
-        <h2>Nenhum ingrediente!</h2>
-      )}
+      <div onClick={() => setViewIngredientCreator(!viewIngredientCreator)}>
+        <h2>Refeição: {props.mealName}</h2>
+        <h2>Descrição: {props.mealDescription}</h2>
+        <h2>Carbohidratos: {props.carbs} g</h2>
+        <h2>Proteínas: {props.proteins} g</h2>
+        <h2>Gorduras: {props.fats} g</h2>
+        <h2>Calorias: {props.kcals} kCal</h2>
+        {props.ingredientList[0] !== null && props.ingredientList.length > 0 ? (
+          props.ingredientList.map((ingredient, index) => (
+            <IngredientTemplate key={index} {...ingredient} />
+          ))
+        ) : (
+          <h2>Nenhum ingrediente!</h2>
+        )}
+      </div>
+      {viewIngredientCreator && <CreateIngredientTemplate mealId={props.mealId} />}
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
   border: solid 1px;
   padding: 5px;
   margin: 5px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
 `;
